b3cb7ff380a6f4e5e4a27fc8a38c726521e1d9c8
TomiCuevas <tomascuevas144@gmail.com>
2025-11-20 18:54:06 -0300

    implementacion de sessionstorage

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,29 @@
+
 // INICIAR SESION
 function loginUser(event) {
     event.preventDefault();
+
+    //Tomar los valores desde el form
+    const email = document.getElementById("email")?.value.trim();
+    const password = document.getElementById("password")?.value.trim();
+
+    if (!email || !password) {
+        alert("Completa todos los campos");
+        return;
+    }
+    // Guardar datos en el sessionStorage 
+    const userData = {
+        email: email,
+        loginTime: new Date().toISOString()
+    };
+    sessionStorage.setItem("loggedUser", JSON.stringify(userData));
     localStorage.setItem("isLoggedIn", "true");
     window.location.href = "./pages/home.html";
 }
 
 // CERRAR SESION
 function logoutUser() {
+    sessionStorage.removeItem("loggedUser");
     localStorage.removeItem("isLoggedIn");
     window.location.href = "../index.html";
 }
-
-
